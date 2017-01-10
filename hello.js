3251a7757cfbc219d5067851284d38f969cbe039
CandiaGu <cagu@mbhs.edu>
2017-01-10 18:40:48 -0500

    Initial commit

---
+++ hello.js
@@ -0,0 +1,17 @@
+function helloWorld(){
+	var x = 5
+	console.log("obama")
+}
+
+helloWorld()
+
+function handleClick() {
+	alert("yep")
+}
+
+
+$(document).ready(function(){
+	//code
+	$(".panel").click(handleClick)
+})
+alert("error")
