23438b51d417fd644a1f3ec13dd7cba45131ad03
CandiaGu <cagu@mbhs.edu>
2017-03-05 22:40:20 -0500

    added scrolling for all images

--- modal.js
+++ modal.js
@@ -7,5 +7,84 @@ $(document).ready(function() {
     }, 1000);
 	});
 
+	$('#p2').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p2d").offset().top
+    }, 1000);
+	});
+	$('#p3').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p3d").offset().top
+    }, 1000);
+	});
+	$('#p4').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p4d").offset().top
+    }, 1000);
+	});
+	$('#p5').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p5d").offset().top
+    }, 1000);
+	});
+	$('#p6').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p6d").offset().top
+    }, 1000);
+	});
+	$('#p7').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p7d").offset().top
+    }, 1000);
+	});
+	$('#p8').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p8d").offset().top
+    }, 1000);
+	});
+	$('#p9').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p9d").offset().top
+    }, 1000);
+	});
+	$('#p10').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p10d").offset().top
+    }, 1000);
+	});
+	$('#p11').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p11d").offset().top
+    }, 1000);
+	});
+	$('#p12').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p12d").offset().top
+    }, 1000);
+	});
+	$('#p13').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p13d").offset().top
+    }, 1000);
+	});
+	$('#p14').click(function(){
+
+		$('html, body').animate({
+        scrollTop: $("#p14d").offset().top
+    }, 1000);
+	});
+
 
 });
