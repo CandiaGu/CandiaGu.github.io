dee93be43de8c12fc8c7ba5fc6d98197fb155827
CandiaGu <cagu@mbhs.edu>
2017-03-05 20:06:20 -0500

    fixed javascript

--- modal.js
+++ modal.js
@@ -1,18 +1,14 @@
-// Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-$('.myImg').click(function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-});
+$(document).ready(function() {
+	$('#myImg').click(function(){
+	    $('#myModal').style.display = "block";
+	    $('#img01').src = this.src;
+	    $('#captionText').innerHTML = this.alt;
+	});
 
 
-// When the user clicks on <span> (x), close the modal
-$(".close").click(function() { 
-  modal.style.display = "none";
+	// When the user clicks on <span> (x), close the modal
+	$(".close").click(function() { 
+	  $('#myModal').style.display = "none";
+	});
 });
